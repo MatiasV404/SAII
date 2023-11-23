c144bf672950f9deaed496e3952a6e7a95d94066
CChavez720 <cam.chavez@duocuc.cl>
2023-11-23 01:52:39 -0300

    Avanze nocturno

--- Interfaz gráfica/login.py
+++ Interfaz gráfica/login.py
@@ -1,11 +1,11 @@
-from customtkinter import CTk, CTkFrame, CTkEntry, CTkLabel, CTkButton, CTkCheckBox, CTkImage
+from customtkinter import *
 from tkinter import PhotoImage
 import customtkinter
 import os
 import tkinter
+from PIL import Image
 
-image = None
-image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Fotos")
+image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
 
 c_blanco = '#FFFFFF'
 c_negro = '#010101'
@@ -13,6 +13,7 @@ c_morado = '#7f5aF0'
 c_verde = '#2cb67d'
 c_calipso = '#69f9ec'
 c_naranjo = '#FF8000'
+c_azul = '#0000FF'
 
 #Inicio de sesion
 
@@ -22,35 +23,46 @@ root.minsize(1280,720)
 root.config(bg = c_blanco)
 root.title('Inicio de sesion')
 
-logo_llama = PhotoImage(file = 'images/llama.png') 
+logo_llama = customtkinter.CTkImage(Image.open(os.path.join(image_path,'llama.png')), size = (200, 200))
 
 frame = CTkFrame(root, fg_color = c_blanco)
 frame.grid(column = 3, row = 3, sticky = 'nsew', padx = 50, pady = 50)
 
 root.columnconfigure(3, weight = 5)
 root.rowconfigure(3, weight = 5)
+'''
+#Texto Iniciar sesion
+texto_iniciar = CTkTextbox(frame, ('sans rerif', 12), text = 'Inicar sesion', )
+texto_iniciar.place(relx = 0.5, rely = 0.3, anchor = tkinter.CENTER)
 
+'''
 #Logo y posicion
-CTkLabel(frame, image = logo_llama).place(relx = 0.5, rely = 0.5, anchor = tkinter.CENTER) 
+logo = customtkinter.CTkLabel(frame, image = logo_llama, text = '')
+logo.place(relx = 0.5, rely = 0.3, anchor = tkinter.CENTER)
 
 #Entrada correo y contraseña
 correo = CTkEntry(frame, font = ('sans rerif', 12), placeholder_text = 'Correo electronico',
-                  border_color = c_blanco, fg_color = c_blanco, width = 220, height = 40)
-correo.place(relx = 0.5, rely = 0.5, anchor = tkinter.CENTER)
+                  border_color = c_negro, fg_color = c_blanco, width = 220, height = 40)
+correo.place(relx = 0.5, rely = 0.6, anchor = tkinter.CENTER)
 
 contrasenia = CTkEntry(frame, font = ('sans rerif', 12), placeholder_text = 'Contraseña',
-                  border_color = c_blanco, fg_color = c_blanco, width = 220, height = 40)
-contrasenia.place(relx = 0.5, rely = 0.6, anchor = tkinter.CENTER)
+                  border_color = c_negro, fg_color = c_blanco, width = 220, height = 40)
+contrasenia.place(relx = 0.5, rely = 0.7, anchor = tkinter.CENTER)
 
 #Recordar contraseña
+'''
 checkbox = CTkCheckBox(frame, text = '¿Olvidaste tu contraseña?', hover_color = c_naranjo,
-                       border_color = c_blanco, fg_color = c_naranjo)
+                       border_color = c_negro, fg_color = c_naranjo, text_color = c_azul)
 checkbox.place(relx = 0.5, rely = 0.7, anchor = tkinter.CENTER)
-
+'''
 #Boton ingresar
-bt_ingresar = CTkButton(frame, font = customtkinter.CTkFont('sans rerif', 12), border_color = c_blanco, fg_color = c_blanco,
-          hover_color = c_naranjo, text = 'Ingresar', height = 40, text_color = c_negro)
-bt_ingresar.place(relx = 0.5, rely = 0.8, anchor = tkinter.CENTER)
+bt_ingresar = CTkButton(frame, font = customtkinter.CTkFont('sans rerif', 12), border_color = c_negro, border_width = 2,
+          hover_color = c_naranjo, fg_color = 'transparent', text = 'Ingresar', height = 40, text_color = c_negro)
+bt_ingresar.place(relx = 0.5, rely = 0.9, anchor = tkinter.CENTER)
 
-root.call('wm', 'iconphoto', root._w, logo_llama)
+#Bonton registrar usuario
+bt_registrar = CTkButton(frame, font = customtkinter.CTkFont('sans rerif', 12), border_color = c_negro, border_width = 2,
+          hover_color = c_naranjo, fg_color = 'transparent', text = 'registrar usuario', height = 40, text_color = c_negro)
+bt_registrar.place(relx = 0.9, rely = 0.9, anchor = tkinter.CENTER)
+
 root.mainloop()
---
+++ Interfaz gráfica/prueba.py
@@ -0,0 +1,24 @@
+import os 
+
+'''
+#Obtener ruta
+ruta_actual = os.path.abspath(__file__)
+
+#navegar
+ruta_vista_bd = os.path.abspath(os.path.join(ruta_actual, "..", ".."))
+
+#Importar imagen
+image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
+
+print(ruta_actual)
+print(ruta_vista_bd)
+print(image_path)
+'''
+from PIL import Image
+from customtkinter import *
+
+my_image = customtkinter.CTkImage(light_image=Image.open("<path to light mode image>"),
+                                  dark_image=Image.open("<path to dark mode image>"),
+                                  size=(30, 30))
+
+image_label = customtkinter.CTkLabel(app, image=my_image, text="")  # display image with a CTkLabel
