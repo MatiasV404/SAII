41de4beb1b6660dac094b3095bf8cba2db1b0730
CChavez720 <cam.chavez@duocuc.cl>
2023-11-24 01:15:09 -0300

    Avanze nocturno

--- Interfaz gráfica/alarmaActivada.py
+++ Interfaz gráfica/alarmaActivada.py
@@ -5,44 +5,51 @@ import os
 import tkinter
 from PIL import Image
 
-image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
+class alarmaActivada(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-c_blanco = '#FFFFFF'
-c_negro = '#010101'
-c_morado = '#7f5aF0'
-c_verde = '#2cb67d'
-c_calipso = '#69f9ec'
-c_naranjo = '#FF8000'
-c_azul = '#0000FF'
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
 
-#Inicio de sesion
-root = CTk()
-root.geometry('1280x720')
-root.minsize(1280,720)
-root.config(bg = c_blanco)
-root.title('S.A.I.I')
+        c_blanco = '#FFFFFF'
+        c_negro = '#010101'
+        c_morado = '#7f5aF0'
+        c_verde = '#2cb67d'
+        c_calipso = '#69f9ec'
+        c_naranjo = '#FF8000'
+        c_azul = '#0000FF'
 
-#Texto iniciar sesion
-'''
-text = label.cget("text")
-'''
+        #Inicio de sesion
+        self.geometry('1280x720')
+        self.minsize(1280,720)
+        self.config(bg = c_blanco)
+        self.title('S.A.I.I')
 
-#Imagenes
-signo_exclamacion = customtkinter.CTkImage(Image.open(os.path.join(image_path,'signoExclamacion.png')), size = (300, 250))
+        #Texto Alarma activada
+        self.alarma_activada = customtkinter.CTkTextbox(master = self, width=650, height = 10, font = ('sans rerif', 75), fg_color = c_blanco,
+                                                        bg_color = c_blanco, text_color = c_negro)
+        self.alarma_activada.place(relx = 0.3, rely = 0.1) 
+        self.alarma_activada.insert("0.0", "¡Alarma Activada!" * 1)
+        self.alarma_activada.configure(state = DISABLED)
 
-frame = CTkFrame(root, fg_color = c_blanco)
-frame.grid(column = 3, row = 3, sticky = 'nsew')
+        #Imagenes
+        self.signo_exclamacion = customtkinter.CTkImage(Image.open(os.path.join(image_path,'signoExclamacion.png')), size = (200, 150))
 
-root.columnconfigure(3, weight = 5)
-root.rowconfigure(3, weight = 5)
+        self.frame = CTkFrame(self, fg_color = c_blanco)
+        self.frame.grid(column = 1, row = 1, sticky = 'nsew')
 
-#Signo de exclamacion y posicion
-signo_exclamacion = customtkinter.CTkLabel(frame, image = signo_exclamacion, text = '')
-signo_exclamacion.place(relx = 0.6, rely = 0.3, anchor = tkinter.NE)
+        #Cantidad de columnas y filas
+        self.columnconfigure(5, weight = 1)
+        self.rowconfigure(5, weight = 1)
 
-#Boton detener
-bt_detener = CTkButton(frame, font = customtkinter.CTkFont('sans rerif', 12), border_color = c_negro, border_width = 2,
-          hover_color = c_naranjo, fg_color = 'transparent', text = 'Detener', height = 60, text_color = c_negro)
-bt_detener.place(relx = 0.54, rely = 0.8, anchor = tkinter.NE)
+        #Signo de exclamacion y posicion
+        self.signo_exclamacion = customtkinter.CTkLabel(self, image = self.signo_exclamacion, text = '', fg_color = c_blanco)
+        self.signo_exclamacion.place(relx = 0.6, rely = 0.3, anchor = tkinter.NE)
 
-root.mainloop()
+        #Boton detener
+        self.bt_detener = CTkButton(self.frame, font = customtkinter.CTkFont('sans rerif', 12), border_color = c_negro, border_width = 2,
+                hover_color = c_naranjo, fg_color = 'transparent', text = 'Detener', height = 60, text_color = c_negro)
+        self.bt_detener.place(relx = 0.5, rely = 0.5, anchor = tkinter.CENTER)
+
+alarmaActivada = alarmaActivada()
+alarmaActivada.mainloop()
